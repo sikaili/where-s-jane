7bfc7efcb0c9803c6b7da2398075e89e3530ef86
sikai <skyl@me.com>
2018-11-27 15:53:21 +0100

    city_size_based_on_freq

--- sketch.js
+++ sketch.js
@@ -6,27 +6,38 @@ let state = -1;
 let doubleClick,ts=[];
 let mic,osc,filt;
 let locss = [];
+let cityA = [];
+let cityArray=[];
 
 function preload() {
-  table = loadTable("assets/e3.csv","csv","header");
+  table = loadTable("assets/e5.csv","csv","header");
 }
 function setup() {
-  for(let e = 0; e < 231; e++){
-    let m = split(table.getString(e,1),",")
-    console.log(m);
+  for(let e = 0; e < 210; e++){
+    let m = split(table.getString(e,1),",");
+    let str = split(table.getString(e,5),",")[0];
+    str = str.split(":")[0];
     let point = {
       n:split(table.getString(e,0),","),
       x:split(table.getString(e,1),","),
       y:split(table.getString(e,2),","),
       place:split(table.getString(e,3),","),
+      city:split(table.getString(e,6),",")[0],
       date:split(table.getString(e,4),","),
       time:split(table.getString(e,5),","),
-
-      size:Math.random()*40
+      hour:str,
+      size:Math.random()*20
     }
-    
     locss.push(point);
+    let mm = point.city;
+    cityA.indexOf(mm)===-1?cityA.push(mm):"";
   }
+  console.log(cityA);
+  // for(let i =0;i<cityA.length;i++){
+  //   cityArray[cityA[i]] = locss.filter(obj=>obj.city===cityA[i]);
+  // } 
+  console.log(cityArray);
+
 	// mic = new p5.AudioIn();
 	// mic.start();
   createCanvas(windowWidth, windowHeight);
@@ -67,49 +78,58 @@ function changeC(){
   return color;
 }
 function draw() {
+  background(200,150);
   let locs = Choose(locss);
+  for(let i =0;i<cityA.length;i++){
+    cityArray[cityA[i]] = locs.filter(obj=>obj.city===cityA[i]);
+  } 
   if(locs){
     push();
-    fill(0,200);
+    fill(0,100);
     strokeWeight(0);
-    textSize(30);
+    textSize(25);
     // console.log(locs[locs.length-1].date);
-    text(locs[locs.length-1].date,width/2,height*0.8);
-    text(locs[locs.length-1].n+" facebook connections",width/2,height*0.8+50+random(0,2));
+    text("Name:\nJane Doe ",0.1*width,0.1*height);
+    text(locs[locs.length-1].date+" "+locs[locs.length-1].time,width/2+50,height*0.8);
+    text(locs[locs.length-1].n+" facebook connections",width/2+50,height*0.8+50+random(0,2));
     if(mouseX>width*0.8){
       fill(150,0,0,random(0,1000))
-      text("Each time you connect,\nFacebook knows where you are",width/2,height*0.8+100+random(0,2));
+      text("Each time you connect,\nFacebook knows where you are",width/2+50,height*0.8+100+random(0,2));
 
     }
     pop();
   }
-  background(200,150);
   beginShape();
   fill(0,0);
   strokeWeight(2-Math.random());
-  stroke(0,100);
+  stroke(0,80);
   for(let i = 0; i<locs.length;i++){
+    vertex(locs[i].x,locs[i].y);
+
     push();
     noStroke();
-    textSize(15+Math.random()/2);
     fill(0,0,100,50);
     ellipse(locs[i].x,locs[i].y,random(5,15)+locs[i].size);
-    fill(255,255,0,80);
-    text(locs[i].time,locs[i].x,locs[i].y);
+    fill(255-cityArray[locs[i].city].length*1,255-cityArray[locs[i].city].length*6,0,20*cityArray[locs[i].city].length);
+    textSize(cityArray[locs[i].city].length+15+Math.random()/2);
+    text(locs[i].city+" ",cityArray[locs[i].city][0].x-50,cityArray[locs[i].city][0].y-cityArray[locs[i].city].length*1);
     pop();
     ellipse(locs[i].x,locs[i].y,random(10,15));
-    vertex(locs[i].x,locs[i].y);
     // curveTightness(3);
   }
+  // console.log(cityArray);
+
   push()
   fill(255,0,0,100);
   ellipse(locs[locs.length-1].x,locs[locs.length-1].y,50-random(25));
-  textSize(30);
+  // textSize(30);
   fill(0,0,0,150);
   stroke(0,0);
-  text(locs[locs.length-1].place,locs[locs.length-1].x+30,locs[locs.length-1].y-60);
+  text(locs[locs.length-1].place,locs[locs.length-1].x-30,locs[locs.length-1].y-30);
   pop();
   endShape();
+  let vmin;
+  width>height?vmin=height:vmin=width;
 }
 
 
@@ -117,7 +137,6 @@ function draw() {
 function Choose(array){
   let n = Math.floor(map(mouseX,0,width,0,array.length))+1;
   return array.slice(0,n);
-
 }
 
 
