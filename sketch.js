1507faa81a5a31cee225bd70fc06008f4f2b706c
sikai <skyl@me.com>
2018-11-28 14:34:26 +0100

    resolve_cityArray_disorder

--- sketch.js
+++ sketch.js
@@ -81,20 +81,21 @@ function draw() {
   for(let i =0;i<placeA.length;i++){
     placeArray[placeA[i]] = locs.filter(obj=>obj.place===placeA[i]);
   }
-  let m = [];
-  m = cityArray.sort((a,b)=>(b.length-a.length));
+  let m = [...cityArray];
+  m = m.sort((a,b)=>(b.length-a.length));
+  // console.log(m==)
   background(150,150,180,100);
-  // bar de navigation
+// bar de navigation
   push();
   stroke(0,0);
   fill(150,50,50,100);
   rect(0,height-15,mouseX,15);
   pop();
-  // general info text
+// general info text
 
   if(locs){
 
-    // info text Right
+// info text Right
     push();
     fill(150,0,0,100);
     strokeWeight(0);
@@ -103,30 +104,30 @@ function draw() {
     console.log()
     if(locs.length<100){
       console.log(m);
-      text("Data of Jane Joe\n\nFrom 03/07/18 to 23/07/18\n\nWe believe you live in:\n\n"+cityA[Math.floor(random(5,20))],0.95*width,0.1*height);
+      text("Data of Jane Joe\n\nFrom 03/07/18 to 23/07/18\n\nWe believe you live in:\n\n"+cityA[Math.floor(random(5,20))],0.95*width,0.1*height+random(1,2));
     }
     else{
-      text("Data of Jane Joe\n\nFrom 03/07/18 to 23/07/18\n\nWe believe you live in:\n\n"+m[0][0].city+" "+m[0][0].place+"\n\n"+m[1][0].place,0.95*width,0.1*height);
+      text("Data of Jane Joe\n\nFrom 03/07/18 to 23/07/18\n\nWe believe you live in:\n\n"+m[0][0].city+" "+m[0][0].place+"\n\n"+m[1][0].place,0.95*width,0.1*height+random(1,2));
 
     }
-
-    // date time Left
+// date time Left
     textAlign(LEFT);
-
     fill(150,0,0,random(0,1000));
     text(locs[locs.length-1].date+" "+locs[locs.length-1].time,width/20+5,height*0.85);
-    text("Total facebook connections:"+locs[locs.length-1].n,width/20+5,height*0.85+50+random(0,2));
+    text("Total facebook connections:"+locs[locs.length-1].n,width/20+5,height*0.85+35+random(0,2));
+
+// provocative sentences
     if(mouseX>width*0.8){
       fill(150,0,0,random(0,1000));
-      text("Each time you connect,\nFacebook knows where you are",width/20+5,height*0.85+100+random(0,2));
+      text("Each time you connect,\nFacebook knows where you are",width/20+5,height*0.85+70+random(0,2));
     }
     pop();
+// Left City List
     if(m.length>10){
       for(let i =0;i<m.length;i++){
         if(!m[i][0]||i>20){
           continue
         }
-
         // console.log(m[i][0].city)
         if(locs[locs.length-1].city==m[i][0].city){
           fill(150,0,0);
@@ -142,15 +143,18 @@ function draw() {
       }
     }
   }
-  // LINES, CIRCLES
+// LINES, CIRCLES
   beginShape();
   fill(0,0);
-  strokeWeight(3-Math.random()*2);
+  strokeWeight(2-Math.random());
   stroke(255,80);
   for(let i = 0; i<locs.length;i++){
-    vertex(locs[i].x,locs[i].y);
+    curveVertex(locs[i].x,locs[i].y);
+    curveTightness(random(1.5,2));
+    // get number of this city
     let d = cityA.indexOf(locs[i].city);
-    // console.log(d);
+
+    // draw parcours points
     push();
     noStroke();
     fill(0,0,100,50);
@@ -158,28 +162,30 @@ function draw() {
     fill(255-cityArray[d].length*1,255-cityArray[d].length*5,0,20*cityArray[d].length);
     textSize(15+Math.random()/2);
     textAlign(CENTER);
-    if(cityArray[d].length>2){
-      fill(0,0,100,3);
+    // Draw frequented cities
+    if(cityArray[d].length>4){
+      fill(0,0,100,10);
+      if(cityArray[d].length>30){
+        fill(0,0,100,2);
+      }
       ellipse(cityArray[d][0].x,cityArray[d][0].y,constrain(cityArray[d].length*8,0,300)+random(-10,10)*cityArray[d].length/2);
-      fill(255-cityArray[d].length*1,255-cityArray[d].length*5,0,8);
-      textSize(constrain(cityArray[d].length*1.5,15,45));
-      text(locs[i].city+" ",cityArray[d][0].x,cityArray[d][0].y-cityArray[d].length);
+      fill(255-cityArray[d].length*1,255-cityArray[d].length*5,0,20);
+      textSize(constrain(cityArray[d].length*1.8,10,45));
+      cityArray[d][0].y>height/2?text(locs[i].city+" ",cityArray[d][0].x,cityArray[d][0].y-cityArray[d].length):text(locs[i].city+" ",cityArray[d][0].x,cityArray[d][0].y+cityArray[d].length*4);
     }
-    // text(locs[i].place+" ",placeArray[locs[i].place][0].x-50,placeArray[locs[i].place][0].y-placeArray[locs[i].place].length*1);
     pop();
     ellipse(locs[i].x,locs[i].y,random(10,15));
-    // curveTightness(3);
   }
-  // console.log(cityArray);
-
+  console.log(cityArray);
   push()
   fill(255,0,0,100);
-  ellipse(locs[locs.length-1].x,locs[locs.length-1].y,50-random(25));
-  fill(100,0,0,150);
-  stroke(0,0);
   textAlign(CENTER);
+  ellipse(locs[locs.length-1].x,locs[locs.length-1].y,50-random(25));
+  fill(150,0,0,150);
+  stroke(0,0);
   textSize(25);
   text(locs[locs.length-1].place,locs[locs.length-1].x,locs[locs.length-1].y+50);
+
   pop();
   endShape();
   let vmin;
