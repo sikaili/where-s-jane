303a7698e3642f46ae7c5c1512c587f043d4d9c2
sikai <skyl@me.com>
2018-11-28 15:00:18 +0100

    change_color_of_frequent_cities

--- sketch.js
+++ sketch.js
@@ -62,7 +62,7 @@ function setup() {
   })
 // MAP TO SCREEN
   locss.map(point=>{
-    point.x = map(point.x,minx,maxx,width*0.9,width*0.1+50);
+    point.x = map(point.x,minx,maxx,width*0.9-50,width*0.1+100);
     point.y = map(point.y,miny,maxy,height*0.23,height*0.9);
   })
 
@@ -85,41 +85,48 @@ function draw() {
   m = m.sort((a,b)=>(b.length-a.length));
   // console.log(m==)
   background(150,150,180,100);
-// bar de navigation
-  push();
-  stroke(0,0);
-  fill(150,50,50,100);
-  rect(0,height-15,mouseX,15);
-  pop();
+
 // general info text
 
   if(locs){
 
 // info text Right
+    const top = 35;
+    const left = width-15;
     push();
-    fill(150,0,0,100);
     strokeWeight(0);
     textSize(15);
     textAlign(RIGHT);
     console.log()
-    if(locs.length<100){
+    if(locs.length<50){
       console.log(m);
-      text("Data of Jane Joe\n\nFrom 03/07/18 to 23/07/18\n\nWe believe you live in:\n\n"+cityA[Math.floor(random(5,20))],0.95*width,0.1*height+random(1,2));
+      fill(0,0,150,80);
+      text("Data of Jane Joe",left,top+random(1,2));
+      text("From 03/07/18 to 23/07/18",left,top+35+random(1,2));
+      text("We believe you live in:",left,top+70+random(1,2));
+      fill(150,0,0,100);
+      text(cityA[Math.floor(random(5,20))],left,top+105+random(1,2));
     }
     else{
-      text("Data of Jane Joe\n\nFrom 03/07/18 to 23/07/18\n\nWe believe you live in:\n\n"+m[0][0].city+" "+m[0][0].place+"\n\n"+m[1][0].place,0.95*width,0.1*height+random(1,2));
+      fill(0,0,150,80);
+      text("Data of Jane Joe",left,top+random(1,2));
+      text("From 03/07/18 to 23/07/18",left,top+35+random(1,2));
+      text("We believe you live in:",left,top+70+random(1,2));
+      fill(150,0,0,100);
+      text(m[0][0].city+" "+m[0][0].place,left,top+105+random(1,2));
+      text(m[1][0].place,left,top+140+random(1,2));
 
     }
 // date time Left
     textAlign(LEFT);
     fill(150,0,0,random(0,1000));
-    text(locs[locs.length-1].date+" "+locs[locs.length-1].time,width/20+5,height*0.85);
-    text("Total facebook connections:"+locs[locs.length-1].n,width/20+5,height*0.85+35+random(0,2));
+    text(locs[locs.length-1].date+" "+locs[locs.length-1].time,15,height*0.85);
+    text("Total facebook connections:"+locs[locs.length-1].n,15,height*0.85+35+random(0,2));
 
 // provocative sentences
     if(mouseX>width*0.8){
       fill(150,0,0,random(0,1000));
-      text("Each time you connect,\nFacebook knows where you are",width/20+5,height*0.85+70+random(0,2));
+      text("Each time you connect,\nFacebook knows where you are",15,height*0.85+70+random(0,2));
     }
     pop();
 // Left City List
@@ -149,7 +156,7 @@ function draw() {
   strokeWeight(2-Math.random());
   stroke(255,80);
   for(let i = 0; i<locs.length;i++){
-    curveVertex(locs[i].x,locs[i].y);
+    vertex(locs[i].x,locs[i].y);
     curveTightness(random(1.5,2));
     // get number of this city
     let d = cityA.indexOf(locs[i].city);
@@ -158,15 +165,15 @@ function draw() {
     push();
     noStroke();
     fill(0,0,100,50);
-    ellipse(locs[i].x,locs[i].y,random(5,15)+locs[i].size);
+    ellipse(locs[i].x,locs[i].y,random(10,20)+locs[i].size);
     fill(255-cityArray[d].length*1,255-cityArray[d].length*5,0,20*cityArray[d].length);
     textSize(15+Math.random()/2);
     textAlign(CENTER);
     // Draw frequented cities
     if(cityArray[d].length>4){
-      fill(0,0,100,10);
+      fill(100,0,0,10);
       if(cityArray[d].length>30){
-        fill(0,0,100,2);
+        fill(100,0,0,2);
       }
       ellipse(cityArray[d][0].x,cityArray[d][0].y,constrain(cityArray[d].length*8,0,300)+random(-10,10)*cityArray[d].length/2);
       fill(255-cityArray[d].length*1,255-cityArray[d].length*5,0,20);
@@ -181,7 +188,7 @@ function draw() {
   fill(255,0,0,100);
   textAlign(CENTER);
   ellipse(locs[locs.length-1].x,locs[locs.length-1].y,50-random(25));
-  fill(150,0,0,150);
+  fill(130,10,10,180);
   stroke(0,0);
   textSize(25);
   text(locs[locs.length-1].place,locs[locs.length-1].x,locs[locs.length-1].y+50);
@@ -190,6 +197,12 @@ function draw() {
   endShape();
   let vmin;
   width>height?vmin=height:vmin=width;
+  // bar de navigation
+  push();
+  stroke(0,0);
+  fill(30,50,150,100);
+  rect(0,height-15,mouseX,15);
+  pop();
 }
 
 
